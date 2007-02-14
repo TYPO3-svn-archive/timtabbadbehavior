3ecb2336fae78f80da0a747b723ecd317296d3cd
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-02-14 09:59:32 +0000

    v0.9.0: initial upload to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtabbadbehavior/trunk@4908 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "timtab_badbehavior"
-# 
-# Auto generated 30-01-2007 12:45
-# 
+#
+# Auto generated 14-02-2007 10:57
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'TIMTAB Bad Behavior',
 	'description' => 'An anti SPAM solution not only for TIMTAB, but every guestbook and the whole TYPO3 system',
 	'category' => 'misc',
@@ -20,7 +21,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -28,9 +29,16 @@ $EM_CONF[$_EXTKEY] = Array (
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'private' => '',
-	'download_password' => '',
-	'version' => '0.0.0',	// Don't modify this! Managed automatically during upload to repository.
+	'version' => '0.9.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:39:{s:9:"ChangeLog";s:4:"8140";s:22:"bad-behavior-typo3.php";s:4:"4ee7";s:33:"class.tx_timtabbadbehavior_bb.php";s:4:"dd6e";s:21:"ext_conf_template.txt";s:4:"851a";s:12:"ext_icon.gif";s:4:"6baf";s:17:"ext_localconf.php";s:4:"96b1";s:14:"ext_tables.php";s:4:"0c47";s:14:"ext_tables.sql";s:4:"dce8";s:33:"icon_tx_timtabbadbehavior_log.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"86d5";s:7:"tca.php";s:4:"f16c";s:26:"bad-behavior/admin.inc.php";s:4:"456f";s:27:"bad-behavior/banned.inc.php";s:4:"2932";s:30:"bad-behavior/blackhole.inc.php";s:4:"97e1";s:30:"bad-behavior/blacklist.inc.php";s:4:"d1c8";s:33:"bad-behavior/common_tests.inc.php";s:4:"fe45";s:25:"bad-behavior/core.inc.php";s:4:"8fc9";s:30:"bad-behavior/functions.inc.php";s:4:"644d";s:27:"bad-behavior/google.inc.php";s:4:"5c2a";s:33:"bad-behavior/housekeeping.inc.php";s:4:"3121";s:23:"bad-behavior/index.html";s:4:"d41d";s:30:"bad-behavior/konqueror.inc.php";s:4:"f678";s:25:"bad-behavior/lynx.inc.php";s:4:"1fe3";s:32:"bad-behavior/movabletype.inc.php";s:4:"1d2e";s:28:"bad-behavior/mozilla.inc.php";s:4:"f95b";s:25:"bad-behavior/msie.inc.php";s:4:"be22";s:27:"bad-behavior/msnbot.inc.php";s:4:"0def";s:26:"bad-behavior/opera.inc.php";s:4:"a56a";s:25:"bad-behavior/post.inc.php";s:4:"f5e4";s:30:"bad-behavior/responses.inc.php";s:4:"da85";s:27:"bad-behavior/safari.inc.php";s:4:"5985";s:29:"bad-behavior/screener.inc.php";s:4:"089a";s:30:"bad-behavior/trackback.inc.php";s:4:"1feb";s:28:"bad-behavior/version.inc.php";s:4:"13b8";s:30:"bad-behavior/whitelist.inc.php";s:4:"6296";s:19:"doc/wizard_form.dat";s:4:"4457";s:20:"doc/wizard_form.html";s:4:"7a3a";s:48:"modfunc1/class.tx_timtabbadbehavior_modfunc1.php";s:4:"431c";s:22:"modfunc1/locallang.xml";s:4:"c150";}',
 );
 
 ?>
