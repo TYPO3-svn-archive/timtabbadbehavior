63f251e3df8b2b42c8270e78480c48e96e57259a
flyguide <flyguide@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-12 07:42:45 +0000

    v1.0.0 from as in TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/timtabbadbehavior/trunk@7474 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "timtab_badbehavior"
 #
-# Auto generated 08-04-2007 14:44
+# Auto generated 12-12-2007 08:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.9.2',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:40:{s:9:"ChangeLog";s:4:"331f";s:22:"bad-behavior-typo3.php";s:4:"4ee7";s:33:"class.tx_timtabbadbehavior_bb.php";s:4:"dd6e";s:21:"ext_conf_template.txt";s:4:"bec4";s:12:"ext_icon.gif";s:4:"6baf";s:17:"ext_localconf.php";s:4:"96b1";s:14:"ext_tables.php";s:4:"0c47";s:14:"ext_tables.sql";s:4:"4f65";s:33:"icon_tx_timtabbadbehavior_log.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"86d5";s:7:"tca.php";s:4:"f16c";s:26:"bad-behavior/admin.inc.php";s:4:"456f";s:27:"bad-behavior/banned.inc.php";s:4:"2932";s:30:"bad-behavior/blackhole.inc.php";s:4:"97e1";s:30:"bad-behavior/blacklist.inc.php";s:4:"d1c8";s:33:"bad-behavior/common_tests.inc.php";s:4:"fe45";s:25:"bad-behavior/core.inc.php";s:4:"8fc9";s:30:"bad-behavior/functions.inc.php";s:4:"644d";s:27:"bad-behavior/google.inc.php";s:4:"5c2a";s:33:"bad-behavior/housekeeping.inc.php";s:4:"3121";s:23:"bad-behavior/index.html";s:4:"d41d";s:30:"bad-behavior/konqueror.inc.php";s:4:"f678";s:25:"bad-behavior/lynx.inc.php";s:4:"1fe3";s:32:"bad-behavior/movabletype.inc.php";s:4:"1d2e";s:28:"bad-behavior/mozilla.inc.php";s:4:"f95b";s:25:"bad-behavior/msie.inc.php";s:4:"be22";s:27:"bad-behavior/msnbot.inc.php";s:4:"0def";s:26:"bad-behavior/opera.inc.php";s:4:"a56a";s:25:"bad-behavior/post.inc.php";s:4:"f5e4";s:30:"bad-behavior/responses.inc.php";s:4:"da85";s:27:"bad-behavior/safari.inc.php";s:4:"5985";s:29:"bad-behavior/screener.inc.php";s:4:"089a";s:30:"bad-behavior/trackback.inc.php";s:4:"1feb";s:28:"bad-behavior/version.inc.php";s:4:"13b8";s:30:"bad-behavior/whitelist.inc.php";s:4:"6296";s:14:"doc/manual.sxw";s:4:"13dd";s:19:"doc/wizard_form.dat";s:4:"4457";s:20:"doc/wizard_form.html";s:4:"7a3a";s:48:"modfunc1/class.tx_timtabbadbehavior_modfunc1.php";s:4:"431c";s:22:"modfunc1/locallang.xml";s:4:"c150";}',
+	'_md5_values_when_last_written' => 'a:40:{s:9:"ChangeLog";s:4:"bdff";s:22:"bad-behavior-typo3.php";s:4:"4ee7";s:33:"class.tx_timtabbadbehavior_bb.php";s:4:"dd6e";s:21:"ext_conf_template.txt";s:4:"bec4";s:12:"ext_icon.gif";s:4:"6baf";s:17:"ext_localconf.php";s:4:"96b1";s:14:"ext_tables.php";s:4:"0c47";s:14:"ext_tables.sql";s:4:"4f65";s:33:"icon_tx_timtabbadbehavior_log.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"86d5";s:7:"tca.php";s:4:"f16c";s:26:"bad-behavior/admin.inc.php";s:4:"1cb6";s:27:"bad-behavior/banned.inc.php";s:4:"2932";s:30:"bad-behavior/blackhole.inc.php";s:4:"f0ad";s:30:"bad-behavior/blacklist.inc.php";s:4:"cefc";s:33:"bad-behavior/common_tests.inc.php";s:4:"fe45";s:25:"bad-behavior/core.inc.php";s:4:"86de";s:30:"bad-behavior/functions.inc.php";s:4:"b027";s:27:"bad-behavior/google.inc.php";s:4:"5c2a";s:33:"bad-behavior/housekeeping.inc.php";s:4:"3121";s:23:"bad-behavior/index.html";s:4:"d41d";s:30:"bad-behavior/konqueror.inc.php";s:4:"f678";s:25:"bad-behavior/lynx.inc.php";s:4:"1fe3";s:32:"bad-behavior/movabletype.inc.php";s:4:"1d2e";s:28:"bad-behavior/mozilla.inc.php";s:4:"f95b";s:25:"bad-behavior/msie.inc.php";s:4:"be22";s:27:"bad-behavior/msnbot.inc.php";s:4:"0def";s:26:"bad-behavior/opera.inc.php";s:4:"a56a";s:25:"bad-behavior/post.inc.php";s:4:"9a64";s:30:"bad-behavior/responses.inc.php";s:4:"da85";s:27:"bad-behavior/safari.inc.php";s:4:"5985";s:29:"bad-behavior/screener.inc.php";s:4:"089a";s:30:"bad-behavior/trackback.inc.php";s:4:"1feb";s:28:"bad-behavior/version.inc.php";s:4:"207a";s:30:"bad-behavior/whitelist.inc.php";s:4:"f724";s:14:"doc/manual.sxw";s:4:"13dd";s:19:"doc/wizard_form.dat";s:4:"4457";s:20:"doc/wizard_form.html";s:4:"7a3a";s:48:"modfunc1/class.tx_timtabbadbehavior_modfunc1.php";s:4:"431c";s:22:"modfunc1/locallang.xml";s:4:"c150";}',
 	'suggests' => array(
 	),
 );
